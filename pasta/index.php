815a2d73cc8112f5b3e15d23ee46140d72f505f8
ConradtCheddar <111157832+ConradtCheddar@users.noreply.github.com>
2025-11-11 09:54:52 -0300

    aividade php3

---
+++ pasta/index.php
@@ -0,0 +1,44 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    minha pagina html
+    <?php
+    echo "ola<BR>" 
+    $nome="Conradt<BR>" 
+    echo $nome."<BR>"
+    ?>
+    <h2> exemplo de tipos de dados</h2>
+    <?php
+    $a=5;
+    $b=7;
+    echo "<p>".$a+$b."<p>";
+    ?>
+    <p> <?php echo .$a*$b.?> </p>
+    <h2>operador ternario</h2>
+    <?php
+    $nota=9.9;
+    $situacao = $nota >= 6 ? 'aprovado' : 'reprovado';
+    echo $situacao;
+    ?>
+    <h2>while</h2>
+    <?php
+    $i = 0;
+    while ($i<10){
+        $i++;
+        echo "i=".$i." - ";
+    }
+    ?>
+    <?php
+    $vetor = array(20,19,22,19,23);
+    for ($posicao=0; $posicao < 5; $posicao++) { 
+        echo $vetor[$posicao] . '<br />'
+    }
+    ?>
+
+    
+</body>
+</html>
